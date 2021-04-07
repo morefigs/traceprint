31f508359ce643f18a09d11ea0358456a7c7e08d
morefigs <morefigs@gmail.com>
2021-04-07 20:12:18 +1000

    Moved example file

--- examples/example.py
+++ examples/example.py
@@ -2,14 +2,14 @@ from printhack import link, unlink, suppress
 
 
 print("this will print normally")
+
 link()
 print("these lines will print")
 print("with a timestamp")
-
-from time import sleep
-sleep(.05)
 print("and a link")
+
 unlink()
 print("this will print normally again")
+
 suppress()
 print("this won't print at all")
