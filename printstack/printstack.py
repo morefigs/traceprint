ed13db7844e056f589bfd8f78c216e5c3319f10d
morefigs <morefigs@gmail.com>
2021-04-10 16:52:15 +1000

    Improved handling of end and newline chars

--- printstack/printstack.py
+++ printstack/printstack.py
@@ -28,30 +28,34 @@ def enable() -> None:
     Adds filename and line number to the builtin print function.
     """
     def _printstack(*objects, sep=' ', end='\n', file=sys.stdout, flush=False) -> None:
-        # Overrides `end` to preserve formatting
-        end = '\n'
 
         # Subtract 1 to hide this function call
         stack_depth = len(stack()) - 1
         if STACK_DEPTH:
             stack_depth = min(stack_depth, STACK_DEPTH)
 
-        # Get the stack trace (without list comprehension, which adds to the stack)
+        # Get the stack trace in reverse, similar to a traceback
         stack_infos = []
         for i in range(stack_depth, 0, -1):
             stack_infos.append(getframeinfo(stack()[i][0]))
 
-        # Prints similar to exception traceback for multiple lines
-        if stack_depth > 1:
-            print_orig()
-            for info in stack_infos:
-                print_orig(f'  {_link_str(info)}')
-            print_orig(*objects, sep=sep, end=end, file=file, flush=flush)
+        # Manually build the regular print string, appending end if not "empty"
+        print_str = sep.join((str(e) for e in objects))
+        print_str += end.strip()
 
         # Prints on a single line if depth is 1
+        if stack_depth == 1:
+
+            # Calculate right justify padding, accounting for possible new lines
+            padding = ' ' * (PADDING - len(print_str.splitlines()[-1]))
+            complete_str = f'{print_str}{padding}{_link_str(stack_infos[0])}'
+
+        # Prints similar to exception traceback for multiple lines
         else:
-            padding = " " * (PAD_WIDTH - len(sep.join((f'{e}' for e in objects))))
-            print_orig(*objects, padding, _link_str(stack_infos[0]), sep=sep, end=end, file=file, flush=flush)
+            link_strs = '\n'.join(f'  {_link_str(info)}' for info in stack_infos)
+            complete_str = f'\n{link_strs}\n{print_str}'
+
+        print_orig(complete_str, file=file, flush=flush)
 
     builtins.print = _printstack
 
