46038faf0607f508840a05dc91c460f2d71626f3
morefigs <morefigs@gmail.com>
2021-04-11 08:45:42 +1000

    Better handling of newlines at end of string

--- printstack/printstack.py
+++ printstack/printstack.py
@@ -20,7 +20,7 @@ def set_options(limit: Optional[int] = None, right_align: Optional[int] = None):
 
 
 def _link_str(info) -> str:
-    return f'File "{info.filename}", line {info.lineno}, in {info.function}'
+    return f'  File "{info.filename}", line {info.lineno}, in {info.function}'
 
 
 def enable() -> None:
@@ -39,25 +39,25 @@ def enable() -> None:
         for i in range(limit, 0, -1):
             stack_infos.append(getframeinfo(stack()[i][0]))
 
-        # Manually build the regular print string, appending end if not "empty"
-        print_str = sep.join((str(e) for e in objects))
-        print_str += end.rstrip()
+        # Manually build the regular print string
+        print_str = sep.join((str(e) for e in objects)) + end
+        # Remove (one) newline end for better formatting
+        if print_str.endswith('\n'):
+            print_str = print_str[:-1]
 
         # Prints on a single line if depth is 1
         if limit == 1:
-
             # Calculate right justify padding, accounting for possible new lines
             if len(print_str) == 0 or print_str.endswith('\n') or print_str.endswith('\r'):
                 width = 0
             else:
                 width = len(print_str.splitlines()[-1])
-            padding = '  ' + ' ' * (RIGHT_ALIGN - width - 2)
-
+            padding = ' ' * (RIGHT_ALIGN - width - 2)
             complete_str = f'{print_str}{padding}{_link_str(stack_infos[0])}'
 
         # Prints similar to exception traceback for multiple lines
         else:
-            link_strs = '\n'.join(f'  {_link_str(info)}' for info in stack_infos)
+            link_strs = '\n'.join(f'{_link_str(info)}' for info in stack_infos)
             complete_str = f'\n{link_strs}\n{print_str}'
 
         print_orig(complete_str, file=file, flush=flush)
