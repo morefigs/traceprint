f3d11f287154ac84143b22ed8e7c88f70538a3ec
morefigs <morefigs@gmail.com>
2022-07-04 20:01:35 +1000

    Updated description

--- traceprint/__init__.py
+++ traceprint/__init__.py
@@ -1,6 +1,6 @@
 """
 traceprint is a Python package that adds stack trace links to the builtin print function, so that editors such as
-PyCharm can link you to the source of the print call.
+PyCharm can link to the source of the print call.
 """
 
 from .traceprint import set_options, enable, disable, suppress
